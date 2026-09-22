1221a43cd1d2118d6f0493e0fa4b90aa3211d1dc
Julio Leite <julio@agenciafrato.com.br>
2026-09-22 11:47:54 -0300

    Implementing basics of langchain with openai

---
+++ src/linkedin_langchain/main.py
@@ -0,0 +1,10 @@
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = init_chat_model(model="gpt-4o-mini",temperature=0.7)
+
+response = model.invoke("O que é o LangChain?")
+
+print(response)

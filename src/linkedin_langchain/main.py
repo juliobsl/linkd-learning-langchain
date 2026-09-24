00ebb352b0bfd1f6e3c13835148cc44995d86310
Julio Leite <julio@agenciafrato.com.br>
2026-09-24 13:14:04 -0300

    Implementing simple tool calling

--- src/linkedin_langchain/main.py
+++ src/linkedin_langchain/main.py
@@ -1,18 +1,40 @@
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
-from langchain.messages import SystemMessage, HumanMessage
+from langchain.messages import HumanMessage
+from langchain.tools import tool
 
 load_dotenv()
 
-model = init_chat_model(model="gpt-4o-mini",temperature=0.7)
+
+@tool
+def calculate_bmi(weight: float, height: float) -> float:
+    """
+    Calculates the Body Mass Index (BMI) given weight in kilograms and height in meters.
+    Arguments:
+        weight (float): The weight of the individual in kilograms.
+        height (float): The height of the individual in meters.
+    Returns:
+        float: The calculated BMI value rounded to two decimal places.
+    """
+    bmi = weight / (height ** 2)
+    return round(bmi, 2)
+
+
+
+model = init_chat_model(model="gpt-4.1-mini",temperature=0.7)
+model_with_tools = model.bind_tools([calculate_bmi])
 
 # historico - chat
 messages = [
-    # instruções, role prompt, etc
-    SystemMessage(content="Você é um assistente que fala português e responde em forma de poemas japanês haiku"),
-    # prompt do usuário
-    HumanMessage(content="Como ser mais produtivo?")
+    HumanMessage(content="imc de uma pessoa com 70 quilos e 175 de altura")
 ]
 
-for chunk in model.stream(messages):
-    print(chunk.content, end="", flush=True)
+response = model_with_tools.invoke(messages)
+messages.append(response)
+
+for tool_call in response.tool_calls:
+    result = calculate_bmi.invoke(tool_call)
+    messages.append(result)
+
+final = model_with_tools.invoke(messages)
+print(final)

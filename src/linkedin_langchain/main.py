a4ed927ef2cd0bda2b7116ba0d5f4b61da97ddeb
Julio Leite <julio@agenciafrato.com.br>
2026-09-24 12:10:27 -0300

    Using message types

--- src/linkedin_langchain/main.py
+++ src/linkedin_langchain/main.py
@@ -1,10 +1,19 @@
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
+from langchain.messages import SystemMessage, HumanMessage
 
 load_dotenv()
 
 model = init_chat_model(model="gpt-4o-mini",temperature=0.7)
 
-response = model.invoke("O que é o LangChain?")
+# historico - chat
+messages = [
+    # instruções, role prompt, etc
+    SystemMessage(content="Você é um assistente que fala português e responde em forma de poemas japanês haiku"),
+    # prompt do usuário
+    HumanMessage(content="Como ser mais produtivo?")
+]
+
+response = model.invoke(messages)
 
 print(response)

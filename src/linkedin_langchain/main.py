9cf3e6685833522dd48c9ac793de08a04db8a657
Julio Leite <julio@agenciafrato.com.br>
2026-09-24 15:41:54 -0300

    Structured responses

--- src/linkedin_langchain/main.py
+++ src/linkedin_langchain/main.py
@@ -2,25 +2,30 @@ from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 from langchain.messages import HumanMessage
 from langchain.agents import create_agent
-from langgraph.checkpoint.memory import InMemorySaver
+from pydantic import BaseModel, Field
+from pprint import pprint
+
+class PersonalInfo(BaseModel):
+    """Schema for personal information of the user."""
+    name: str = Field(description="Full name of the user")
+    age: int = Field(description="Age of the user")
+    profession: str = Field(description="Profession of the user")
 
 load_dotenv()
 
 model = init_chat_model(model="gpt-4.1-mini", temperature=0.7)
-agent = create_agent(model=model, checkpointer=InMemorySaver())
-
-config = {
-    "configurable": {"thread_id": "user-123"}
-}
+agent = create_agent(model=model, response_format=PersonalInfo)
 
 response = agent.invoke(
-    {"messages": [HumanMessage(content="Oi, meu nome é julio")]},
-    config=config
+    {
+        "messages": [HumanMessage(content="Extraia os dados: Julio Leite, 26, Dev senior")]
+    }
 )
-print(response["messages"][-1].content)
 
-response = agent.invoke(
-    {"messages": [HumanMessage(content="Qual é o meu nome?")]},
-    config=config
-)
-print(response["messages"][-1].content)
+pprint(response)
+print("================================")
+
+personal_info = response["structured_response"]
+print(personal_info.name)
+print(personal_info.age)
+print(personal_info.profession)
